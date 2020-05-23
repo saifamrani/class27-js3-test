f37bd086af8b6022a3035e9b999b6b58e08a678b
Saif Alamrani <saifamrani@icloud.com>
2020-05-23 12:03:29 +0200

    finished the test Saif Alamrani

--- version3/test.js
+++ version3/test.js
@@ -8,6 +8,22 @@ Write a function that:
 - Makes use of async/await syntax
 - Display the "name", "email" and "city" of the first person inside an unordered list.
 */
+async function fetchJSON(URL) {
+  const response = await fetch(URL);
+  const data = await response.json();
+  const ul = document.createElement('ul');
+  const body = document.body;
+  body.appendChild(ul);
+  data.results.forEach((element, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<li>${element.name.first}</li>
+    <li>${element.email}</li>
+    <li>${element.location.city}</li>`;
+    ul.appendChild(li);
+  });
+}
+fetchJSON("https://randomuser.me/api?results=5");
+
 
 /*
 2. 
@@ -17,6 +33,12 @@ a) Implement the following JavaScript library: https://www.chartjs.org/
 
 b) Explain in 100 words or less your approach for implementing the library
 */
+// to use this library we have to use js file of the library or we have to install it by "npm install chart.js --save"
+//  then I add the script src 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js' to My HTML doc. 
+// at the end of body element.
+// Then I create canvas with "myChart" id on my HTML file
+// Then I defined new Chart variable to display my Bar chart.
+// Then I can use the details on my HTML file. 
 
 /*
 3.
@@ -25,11 +47,36 @@ a) Create a class, called Car, that includes:
 - "model", "color", "seats" properties
 - 1 additional method, called "getModel", which returns the model
 
+
 b) Instantiate the class, and give it the name and attributes of a random animal
 
-c) Explain how this class relates Object-Oriented Programming (OOP) in 100 words or less
-*/
+c) Explain how this class relates Object-Oriented Programming (OOP) in 100 words or less:
+answer : 
+Object Oriented Programming (OOP) stand for using self-contained pieces of code to develop applications. 
+known as these self-contained pieces of code objects.
+There are many ways to create a new object in JavaScript like : 
+ object literal,
+ factory functions,
+ constructor functions
+ and classes.
+then creat constructor method to add properties of Car class.
+then create a getter method to return the model property.
+finally we lunch the class, This process called instantiation 
 
+
+*/
+class Car {
+  constructor(model, color, seats) {
+    this._model = model;
+    this.color = color;
+    this.seats = seats;
+  }
+
+  get model() {
+    return this._model;
+  }
+}
+const animalCar = new Car("TESLA model X", "red", 4);
 /*
 4.
 Write a function that:
@@ -42,7 +89,14 @@ Expected output: ["Cookies", "Flowers", "Zebras"]
 
 Make use of the following array:
 */
-const fruits = ['Strawberry', 'Apple', 'Tangerine', 'Banana', 'Melon', 'Pear'];
+
+function sortedFruit() {
+  const fruits = ['Strawberry', 'Apple', 'Tangerine', 'Banana', 'Melon', 'Pear'];
+  fruits.sort();
+  console.log(fruits);
+}
+sortedFruit();
+
 
 /*
 5.
@@ -52,12 +106,35 @@ Using JavaScript only (adding HTML to index.html is NOT allowed), create a funct
 - When the button is clicked, inserts an image URL into the <img> tag and remove the button
 - Uses the following image URL: https://avatars3.githubusercontent.com/u/20858568?s=200&v=4
 */
+const body = document.body;
+const button = document.createElement('button');
+body.appendChild(button);
+button.innerText = "click me!";
+const img = document.createElement('img');
+body.appendChild(img);
+const url = "https://avatars3.githubusercontent.com/u/20858568?s=200&v=4";
+img.setAttribute('src', url);
+img.style.display = "none";
 
-/* 
-6. 
+function showImageByClick() {
+  if (img.style.display === "none") {
+    img.style.display = "block";
+  } else {
+    img.style.display = "none";
+  }
+}
+button.addEventListener("click", showImageByClick);
+/*
+6.
 Answer the following questions:
-- What is an Application Programming Interface (API)? 
-- How does this relate to your HackYourRepo project?
+- What is an Application Programming Interface (API)?
 
-Explain each in 200 words or less. 
+// An API stand for (Application Programming Interface) which is any software that contains a part that's can be accessible from an outside source open to requests from the client. That part that's accessible is the "interface" of that software. called the API and there is two types (public API) and (private API : witch is commonly used).
+
+- How does this relate to your HackYourRepo project?
+//
+I have used github api in HackYourRepo project.
+I recieved the responses from it by using AJAX. Then I HAVE displayed the details in my HTML file dynamically.
+
+Explain each in 200 words or less.
 */
